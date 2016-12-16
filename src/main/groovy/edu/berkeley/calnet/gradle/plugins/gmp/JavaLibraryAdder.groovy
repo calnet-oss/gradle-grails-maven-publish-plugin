2f8d9948d993fe9383217f6659bf6a21d87a1310
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2016-12-16 10:17:21 -0800

    Reformatting.

--- src/main/groovy/edu/berkeley/calnet/gradle/plugins/gmp/JavaLibraryAdder.groovy
+++ src/main/groovy/edu/berkeley/calnet/gradle/plugins/gmp/JavaLibraryAdder.groovy
@@ -20,29 +20,29 @@
 package edu.berkeley.calnet.gradle.plugins.gmp
 
 import org.gradle.api.Project
-import org.gradle.api.plugins.JavaPluginConvention
-import org.gradle.api.tasks.bundling.Jar
-import org.gradle.api.internal.artifacts.publish.ArchivePublishArtifact
 import org.gradle.api.artifacts.Configuration
+import org.gradle.api.internal.artifacts.publish.ArchivePublishArtifact
+import org.gradle.api.internal.java.JavaLibrary
 import org.gradle.api.internal.plugins.DefaultArtifactPublicationSet
 import org.gradle.api.plugins.JavaPlugin
-import org.gradle.api.internal.java.JavaLibrary
+import org.gradle.api.plugins.JavaPluginConvention
 import org.gradle.api.tasks.SourceSet
+import org.gradle.api.tasks.bundling.Jar
 
 abstract class JavaLibraryAdder {
     // see Gradle JavaPlugin.configureArchivesAndComponent() (v3.2.1)
     static void addJavaLibrary(final Project project) {
-        JavaPluginConvention pluginConvention = project.getConvention().getPlugin(JavaPluginConvention.class)
-        
-        Jar jar = project.getTasks().create(JavaPlugin.JAR_TASK_NAME, Jar.class)
+        JavaPluginConvention pluginConvention = project.convention.getPlugin(JavaPluginConvention)
+
+        Jar jar = project.tasks.create(JavaPlugin.JAR_TASK_NAME, Jar)
         jar.setDescription("Assembles a jar archive containing the main classes.")
-        jar.from(pluginConvention.getSourceSets().getByName(SourceSet.MAIN_SOURCE_SET_NAME).getOutput())
-        
+        jar.from(pluginConvention.sourceSets.getByName(SourceSet.MAIN_SOURCE_SET_NAME).output)
+
         ArchivePublishArtifact jarArtifact = new ArchivePublishArtifact(jar)
-        Configuration runtimeConfiguration = project.getConfigurations().getByName(JavaPlugin.RUNTIME_CONFIGURATION_NAME)
-        
-        runtimeConfiguration.getArtifacts().add(jarArtifact)
-        project.getExtensions().getByType(DefaultArtifactPublicationSet.class)addCandidate(jarArtifact)
-        project.getComponents().add(new JavaLibrary(jarArtifact, runtimeConfiguration.getAllDependencies()))
-    }    
+        Configuration runtimeConfiguration = project.configurations.getByName(JavaPlugin.RUNTIME_CONFIGURATION_NAME)
+
+        runtimeConfiguration.artifacts.add(jarArtifact)
+        project.extensions.getByType(DefaultArtifactPublicationSet).addCandidate(jarArtifact)
+        project.components.add(new JavaLibrary(jarArtifact, runtimeConfiguration.allDependencies))
+    }
 }
